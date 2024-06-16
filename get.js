66099c3bc8e12636e3d6df75b778deb91500622b
umar545 <muhammadumarfarooq545@gmail.com>
2024-06-16 12:52:24 +0500

    added basic setup

--- get.js
+++
@@ -1,4 +0,0 @@
-console.log("object");
-fetch("http://localhost:3000/login")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
--- todos/product.models.js
+++ todos/product.models.js
@@ -31,6 +31,11 @@ const productSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+    status: {
+      type: String,
+      enum: ["pending", "ordering", "sending"],
+      default: "pending",
+    },
   },
   { timestamps: true }
 );
